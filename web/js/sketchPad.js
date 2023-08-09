b40e6a22fcd019a87dd990b897a1d0a152cd8f4c
Mathias Johansson <hej@mathiasjohansson.se>
2023-08-09 18:39:40 +0200

    Added undo button

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -9,6 +9,13 @@ class SketchPad {
         `
     container.appendChild(this.canvas)
 
+    const lineBreak = document.createElement('br')
+    container.appendChild(lineBreak)
+
+    this.undoBtn = document.createElement('button')
+    this.undoBtn.innerHTML = 'UNDO'
+    container.appendChild(this.undoBtn)
+
     this.ctx = this.canvas.getContext('2d')
 
     this.paths = []
@@ -44,6 +51,10 @@ class SketchPad {
     }
     this.canvas.ontouchend = (e) => {
       this.canvas.onmouseup()
+    }
+    this.undoBtn.onclick = () => {
+      this.paths.pop()
+      this.#redraw()
     }
   }
 
